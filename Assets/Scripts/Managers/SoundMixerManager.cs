095b44c441d088915f84795d764b2f38d6a4f6b4
Sol-Aureus <ejhpepper@gmail.com>
2025-07-17 23:01:37 -0400

    Added music
    - Music now plays in the backgorund

--- Assets/Scripts/Managers/SoundMixerManager.cs
+++ Assets/Scripts/Managers/SoundMixerManager.cs
@@ -44,8 +44,6 @@ public class SoundMixerManager : MonoBehaviour
         // Convert 0 to 10 range to -80 to 0 dB range
         level = Mathf.Log10((level + 0.00001f) / 10) * 20;
         audioMixer.SetFloat("MusicVolume", level);
-
-        SoundFXManager.main.PlaySound(volumeChange, transform, 1f); // Play sound effect when volume changes
     }
 
     // Method to set the narration volume level
